a0417c03b91675ac5c330c9b8d1515e857f72011
nrdrina <167564982+nrdrina@users.noreply.github.com>
2026-04-29 18:05:12 +0800

    switch to sqlite for deployment

--- biodata_project/biodata_project/settings.py
+++ biodata_project/biodata_project/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'biodata_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'biodata_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'biodata_db',
+        # 'USER': 'root',
+        # 'PASSWORD': 'root',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
